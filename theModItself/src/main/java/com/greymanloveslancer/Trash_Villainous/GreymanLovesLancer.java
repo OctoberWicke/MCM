cd9204ce4114b319d01a453037d348fef6232e93
Founded1992 <Founded1991@gmail.com>
2023-03-20 21:10:52 -0400

    Filled out several mod-specific details
    Changed names from examplemod to greymanloveslancer, and filled out relevant information.

--- theModItself/src/main/java/com/greymanloveslancer/Trash_Villainous/GreymanLovesLancer.java
+++ theModItself/src/main/java/com/greymanloveslancer/Trash_Villainous/GreymanLovesLancer.java
@@ -10,9 +10,9 @@ import org.apache.logging.log4j.Logger;
 @Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
 public class ExampleMod
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "greymanloveslancer";
+    public static final String NAME = "Greyman Loves Lancer";
+    public static final String VERSION = "0.0.0";
 
     private static Logger logger;
 
