10a9ff96d41f9cb55ec1e7da97af634b089971b5
Founded1992 <Founded1991@gmail.com>
2023-03-20 21:15:59 -0400

    Small patch for previous filename updates
    corrects compiler-specific information changes.

--- theModItself/src/main/java/com/greymanloveslancer/Trash_Villainous/GreymanLovesLancer.java
+++ theModItself/src/main/java/com/greymanloveslancer/Trash_Villainous/GreymanLovesLancer.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.greymanloveslancer.Trash_Villainous;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
@@ -7,8 +7,8 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = GreymanLovesLancer.MODID, name = GreymanLovesLancer.NAME, version = GreymanLovesLancer.VERSION)
+public class GreymanLovesLancer
 {
     public static final String MODID = "greymanloveslancer";
     public static final String NAME = "Greyman Loves Lancer";
